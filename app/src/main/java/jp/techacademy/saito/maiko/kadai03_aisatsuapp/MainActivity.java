f11ecb0161b5f2f5f8dc1fecc00439232ed78b76
maikokko0317 <ukko_ukko@hotmail.com>
2018-11-29 11:43:22 +0900

    課題提出

--- app/src/main/java/jp/techacademy/saito/maiko/kadai03_aisatsuapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/saito/maiko/kadai03_aisatsuapp/MainActivity.java
@@ -1,7 +1,6 @@
 package jp.techacademy.saito.maiko.kadai03_aisatsuapp;
 
 import android.app.TimePickerDialog;
-import android.content.DialogInterface;
 import android.util.Log;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +11,9 @@ import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView mTextView;
+    String aisatsu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +21,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.textView);
     }
 
     @Override
     public void onClick(View v) {
-        showTimePickerDialog()
+        showTimePickerDialog();
     }
 
     private void showTimePickerDialog() {
@@ -32,6 +36,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                         Log.d("UI_PARTS", String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                        if (hourOfDay >= 2 && hourOfDay <= 9) {
+                            //Log.d("UI_PARTS", "おはよう");
+                            aisatsu = " おはよう";
+                        } else if (hourOfDay >= 10 && hourOfDay <= 17) {
+                            //Log.d("UI_PARTS", "こんにちは");
+                            aisatsu = " こんにちは";
+                        } else if ((hourOfDay >= 18 && hourOfDay <= 24) || (hourOfDay >= 0 && hourOfDay <= 1)) {
+                            //Log.d("UI_PARTS", "こんばんは");
+                            aisatsu = " こんばんは";
+                        }
+                        mTextView.setText(aisatsu);
                     }
                 },
                 13, // 初期値（時間）
