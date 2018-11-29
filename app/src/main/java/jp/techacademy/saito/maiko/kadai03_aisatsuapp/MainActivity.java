c56190e3cfbf52337046dd78e0504484f9a0244a
maikokko0317 <ukko_ukko@hotmail.com>
2018-11-29 11:13:18 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/saito/maiko/kadai03_aisatsuapp/MainActivity.java
@@ -0,0 +1,42 @@
+package jp.techacademy.saito.maiko.kadai03_aisatsuapp;
+
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        showTimePickerDialog()
+    }
+
+    private void showTimePickerDialog() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Log.d("UI_PARTS", String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
+                    }
+                },
+                13, // 初期値（時間）
+                0,  // 初期値（分）
+                true);
+        timePickerDialog.show();
+    }
+}
